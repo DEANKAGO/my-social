a9fca7e7602db4b725b2504d008a0cf2839366fa
deankago <martokhago@gmail.com>
2023-11-13 10:20:18 -0500

    working on share component, imported to feedback component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -13,31 +13,62 @@ const Sidebar = () => {
                 </li>
                 <li className='sideListItem'>
                     < Chat className='sideIcon'/>
-                    <span className="sideListItemText">Feed</span>
+                    <span className="sideListItemText">Chats</span>
                 </li>
                 <li className='sideListItem'>
                     < PlayCircle className='sideIcon'/>
-                    <span className="sideListItemText">Feed</span>
+                    <span className="sideListItemText">Videos</span>
                 </li>
                 <li className='sideListItem'>
                     < Groups className='sideIcon'/>
-                    <span className="sideListItemText">Feed</span>
+                    <span className="sideListItemText">Groups</span>
                 </li>
                 <li className='sideListItem'>
                     < Bookmarks className='sideIcon'/>
-                    <span className="sideListItemText">Feed</span>
+                    <span className="sideListItemText">Bookmarks</span>
                 </li>
                 <li className='sideListItem'>
                     < Work className='sideIcon'/>
-                    <span className="sideListItemText">Feed</span>
+                    <span className="sideListItemText">Jobs</span>
                 </li>
                 <li className='sideListItem'>
                     < Event className='sideIcon'/>
-                    <span className="sideListItemText">Feed</span>
+                    <span className="sideListItemText">Events</span>
                 </li>
                 <li className='sideListItem'>
                     < HelpOutline className='sideIcon'/>
-                    <span className="sideListItemText">Feed</span>
+                    <span className="sideListItemText">Questions</span>
+                </li>
+            </ul>
+            <button className='sideButton'>Show Nore</button>
+            <hr className='sideHr'/>
+            <ul className='sideFriendList'>
+                <li className='sideFriend'>
+                    <img className='sideFriendImg' src="/assets/social-sidebar.jpg" alt="" />
+                    <span className="sideFriendName">Kago Kago</span>
+                </li>
+                <li className='sideFriend'>
+                    <img className='sideFriendImg' src="/assets/social-sidebar.jpg" alt="" />
+                    <span className="sideFriendName">Kago Kago</span>
+                </li>
+                <li className='sideFriend'>
+                    <img className='sideFriendImg' src="/assets/social-sidebar.jpg" alt="" />
+                    <span className="sideFriendName">Kago Kago</span>
+                </li>
+                <li className='sideFriend'>
+                    <img className='sideFriendImg' src="/assets/social-sidebar.jpg" alt="" />
+                    <span className="sideFriendName">Kago Kago</span>
+                </li>
+                <li className='sideFriend'>
+                    <img className='sideFriendImg' src="/assets/social-sidebar.jpg" alt="" />
+                    <span className="sideFriendName">Kago Kago</span>
+                </li><li className='sideFriend'>
+                    <img className='sideFriendImg' src="/assets/social-sidebar.jpg" alt="" />
+                    <span className="sideFriendName">Kago Kago</span>
+                </li>
+                <li className='sideFriend'>
+                    <img className='sideFriendImg' src="/assets/social-sidebar.jpg" alt="" />
+                    <span className="sideFriendName">Kago Kago</span>
                 </li>
             </ul>
         </div>
