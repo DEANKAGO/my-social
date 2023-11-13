53dc2c31ddf58d2088d38a481fe3cea3f691b81c
deankago <martokhago@gmail.com>
2023-11-13 07:04:41 -0500

    finished on styling the navigation bar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -10,29 +10,29 @@ const Topbar = () => {
         </div>
         <div className="topCenter">
             <div className="searchbar">
-                <Search/>
+                <Search className='searchIcon'/>
                 <input type="text" className='searchInput' />
             </div>
         </div>
         <div className="topRight">
             <div className="topLinks">
-                <span className='topLink'>2</span>
-                <span className='topLink'>3</span>
-                <span className='topLink'>4</span>
+                <span className='topLink'>Homepage</span>
+                <span className='topLink'>Timeline</span>
             </div>
             <div className="topIcons">
                 <div className="topIconItems">
                     <Person />
-                    <span className="topIconBadge"></span>
+                    <span className="topIconBadge">21</span>
                 </div>
                 <div className="topIconItems">
                     <Chat/>
-                    <span className="topIconBadge"></span>
+                    <span className="topIconBadge">7</span>
                 </div><div className="topIconItems">
                     <Notifications />
-                    <span className="topIconBadge"></span>
+                    <span className="topIconBadge">5</span>
                 </div>
             </div>
+            <img src="/assets/social-profile.jpg" alt="" className="topImg" />
         </div>
     </div>
   )
