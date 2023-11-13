42fabd0000be180d916cb98c1f7e1b692bb220a5
deankago <martokhago@gmail.com>
2023-11-13 11:29:23 -0500

    finished on styling share component UI

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import { FolderCopy, Label, LocationOn, InsertEmoticon } from '@mui/icons-material';
 import './share.css'
 
 const Share = () => {
   return (
-    <div>Share</div>
+    <div className='share'>
+        <div className="shareWrapper">
+            <div className="shareTop">
+                <img 
+                className='profileImg' 
+                src="/assets/social-profile.jpg" 
+                alt="Profile Image" 
+                />
+                <input 
+                    className='shareInput'
+                    placeholder='How is the weather today Kago?'
+                />
+            </div>
+            <hr className="shareHr" />
+            <div className="shareBottom">
+                <div className="shareOptions">
+                    <div className="option">
+                        <FolderCopy htmlColor='tomato' className='shareIcon'/>
+                        <span className="optionText">Photo or Video</span>
+                    </div>
+                    <div className="option">
+                        <Label htmlColor='blue' className='shareIcon'/>
+                        <span className="optionText">Photo or Video</span>
+                    </div>
+                    <div className="option">
+                        <LocationOn htmlColor='green' className='shareIcon'/>
+                        <span className="optionText">Photo or Video</span>
+                    </div>
+                    <div className="option">
+                        <InsertEmoticon htmlColor='goldenrod' className='shareIcon'/>
+                        <span className="optionText">Photo or Video</span>
+                    </div>
+                </div>
+                <button className='btn'>Share</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
