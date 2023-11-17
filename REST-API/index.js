6ed3804e14fb153ba1eb37274b18f23e5736d4d5
deankago <martokhago@gmail.com>
2023-11-17 11:22:19 -0500

    added routes

--- REST-API/index.js
+++ REST-API/index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
 
@@ -18,12 +20,13 @@ async function connect() {
 }
 connect();
 
-// mongoose.connect(
-//     process.env.MONGO_URL,).then(() => {
-//         console.log('connected to DB$$$$');
-//     }).catch((error) => {
-//         console.log('error connecting to DB$$$$:', error.message);
-//     });
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
 
 
 app.listen(8800, () => {
---
+++ REST-API/routes/users.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.send('hey hey hey itsjvdb fhkn hdfbv')
+});
+
+
+module.exports = router
