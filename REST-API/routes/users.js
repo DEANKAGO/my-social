fd5052a397be6b0e892498520dbfd73ddd1d619d
deankago <martokhago@gmail.com>
2023-11-17 15:44:13 -0500

    added and tested delete function

--- REST-API/routes/users.js
+++ REST-API/routes/users.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcrypt");
 
 // update user
 router.put('/:id', async (req, res) => {
-    if(req.body.userId === req.params.id || req.user.isAdmin) {
+    if(req.body.userId === req.params.id || req.body.isAdmin) {
         if (req.body.password) {
             try {
                 const salt = await bcrypt.genSalt(10);
@@ -26,7 +26,20 @@ router.put('/:id', async (req, res) => {
         return res.status(403).json('You can update only your account!');
         }
 });
+
 // delete user
+router.delete('/:id', async (req, res) => {
+    if(req.body.userId === req.params.id || req.body.isAdmin) {
+        try {
+            await User.findByIdAndDelete(req.body.id);
+            res.status(200).json('Account deleted');
+        }catch (err) {
+            return res.status(500).json(err);
+        }
+    } else {
+        return res.status(403).json('You can delete only your accounts');
+    }
+});
 // get user
 // follow user
 // unfollow user
