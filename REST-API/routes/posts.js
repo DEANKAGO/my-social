12b03c091a1b3c918165d4a7ea0fd63e6a108a63
deankago <martokhago@gmail.com>
2023-11-22 07:34:21 -0500

    added and tested like function

--- REST-API/routes/posts.js
+++ REST-API/routes/posts.js
@@ -50,6 +50,20 @@ router.delete('/:id', async (req, res) => {
 });
 
 // like/ dislike a post
+
+router.put('/:id/like', async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        if(!post.likes.includes(req.body.userId)) {
+            await Post.updateOne({$push: {likes: req.body.userId}});
+            res.status(200).json('post has been liked successfully');
+        } else {
+            await Post.updateOne({$pull: {likes: req.body.userId}});
+            res.status(200).json('post has been disliked successfully');
+        }
+    } catch (err) { res.status(500).json(err);}
+});
+
 // get a post
 // get timeline posts
 
